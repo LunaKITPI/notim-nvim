9627b4e1787d46c79eae35bb62bd93c5d56f3cb1
LunaKitPI <117634731+LunaKITPI@users.noreply.github.com>
2023-11-19 22:16:15 +0100

    Delete lua/custom directory

--- lua/custom/config.lua
+++
@@ -1,3 +0,0 @@
-vim.cmd("colorscheme kanagawa-wave") -- kanagawa is the default theme, the flavours for this are : lotus, wave, dragon | lotus is a light theme, the other 2 are dark themes
--- this is the user personal config file,  changes made here overwrite the main setup ( for example if you type "vim.opt.number = false" the numbers will dissapear, no need to change to root dir)
-
