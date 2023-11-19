da37b6368acefd543b63c70623938a44040a3d93
LunaKitPI <117634731+LunaKITPI@users.noreply.github.com>
2023-11-19 22:14:53 +0100

    Delete opts directory

--- opts/opts.lua
+++
@@ -1,9 +0,0 @@
-require("opts.oil")
-require("opts.kanagawa")
-require("opts.lualine")
-require("opts.buffer_manager")
-require("opts.dashboard-nvim")
-require("opts.bookmarks")
-require("opts.treesitter")
-require("opts.whichkey")
-
